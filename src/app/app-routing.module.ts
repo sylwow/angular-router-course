f2f226529690509cbcc2c404e722d4c8d8b09a4c
Sylwow <Sylwo1003@gmail.com>
2022-02-14 18:50:36 +0100

    add first routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,36 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CourseComponent } from './courses/course/course.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
-
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'courses',
+    component: CourseComponent,
+  },
+  // {
+  //   path: 'home',
+  //   component: HomeComponent,
+  // },
+  // {
+  //   path: '**',
+  //   redirectTo: '/home',
+  // },
 ];
 
 @NgModule({
   imports: [
-
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule],
   providers: [
